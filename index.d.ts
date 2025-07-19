c81d8809b71fa438529289b69d49ddb647950fb6
kareemaboueid <kareem_aboueid@outlook.com>
2025-07-19 03:03:08 +0300

    feat: update index files to include omitit and flattenit, and enhance documentation for utility functions

--- index.d.ts
+++ index.d.ts
@@ -1,37 +1,52 @@
 /**
  * objeasy - A lightweight utility toolkit to control and manage plain JavaScript objects with precision.
+ *
+ * This package provides six powerful utility functions for object manipulation:
+ * - pickit: Extract specific keys from objects
+ * - modifyit: Modify or erase object properties
+ * - mergeit: Merge multiple objects with deep merging support
+ * - transformit: Transform object values using mapper functions
+ * - omitit: Remove specific keys from objects (inverse of pickit)
+ * - flattenit: Flatten nested objects to single-level with dot notation
+ *
  * @version 2.0.0
  * @author Kareem Aboueid
  * @license MIT
+ * @homepage https://github.com/kareemaboueid/objeasy
  */
 
+// Named exports
 export { default as pickit, PickitOptions } from './src/pickit/pickit';
 export { default as modifyit, ModifyitOptions } from './src/modifyit/modifyit';
 export { default as mergeit, MergeitOptions } from './src/mergeit/mergeit';
-export {
-  default as validateit,
-  ValidateitOptions,
-  ValidationResult,
-  ValidationError,
-  Schema
-} from './src/validateit/validateit';
 export { default as transformit, TransformitOptions, MapperFunction } from './src/transformit/transformit';
+export { default as omitit, OmititOptions } from './src/omitit/omitit';
+export { default as flattenit } from './src/flattenit/flattenit';
 
+// Import functions for default export
 import pickit from './src/pickit/pickit';
 import modifyit from './src/modifyit/modifyit';
 import mergeit from './src/mergeit/mergeit';
-import validateit from './src/validateit/validateit';
 import transformit from './src/transformit/transformit';
+import omitit from './src/omitit/omitit';
+import flattenit from './src/flattenit/flattenit';
 
 /**
  * Default export containing all utility functions
  */
 declare const _default: {
+  /** Extract specific keys from objects with type safety */
   pickit: typeof pickit;
+  /** Modify or erase object properties with flexible options */
   modifyit: typeof modifyit;
+  /** Merge multiple objects with deep merging support and array strategies */
   mergeit: typeof mergeit;
-  validateit: typeof validateit;
+  /** Transform object values using mapper functions with key filtering */
   transformit: typeof transformit;
+  /** Remove specific keys from objects (inverse of pickit) */
+  omitit: typeof omitit;
+  /** Flatten nested objects to single-level with dot notation */
+  flattenit: typeof flattenit;
 };
 
 export default _default;
--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 /**
  * objeasy - A lightweight utility toolkit to control and manage plain JavaScript objects with precision.
+ *
+ * This package provides six powerful utility functions for object manipulation:
+ * - pickit: Extract specific keys from objects
+ * - modifyit: Modify or erase object properties
+ * - mergeit: Merge multiple objects with deep merging support
+ * - transformit: Transform object values using mapper functions
+ * - omitit: Remove specific keys from objects (inverse of pickit)
+ * - flattenit: Flatten nested objects to single-level with dot notation
+ *
  * @version 2.0.0
  * @author Kareem Aboueid
  * @license MIT
+ * @homepage https://github.com/kareemaboueid/objeasy
  */
 
 const pickit = require('./src/pickit/pickit');
 const modifyit = require('./src/modifyit/modifyit');
 const mergeit = require('./src/mergeit/mergeit');
-const validateit = require('./src/validateit/validateit');
 const transformit = require('./src/transformit/transformit');
+const omitit = require('./src/omitit/omitit');
+const flattenit = require('./src/flattenit/flattenit');
 
+// Named exports for CommonJS
 module.exports = {
   pickit,
   modifyit,
   mergeit,
-  validateit,
-  transformit
+  transformit,
+  omitit,
+  flattenit
 };
 
+// Default export for ES6 modules compatibility
 exports.default = {
   pickit,
   modifyit,
   mergeit,
-  validateit,
-  transformit
+  transformit,
+  omitit,
+  flattenit
 };
