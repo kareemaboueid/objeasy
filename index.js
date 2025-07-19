ea97b09e0c32064c9311ed08be44797dd7053c02
kareemaboueid <kareem_aboueid@outlook.com>
2025-07-19 03:11:10 +0300

    chore: remove detailed function descriptions from documentation comment in index.js

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 /**
  * objeasy - A lightweight utility toolkit to control and manage plain JavaScript objects with precision.
  *
- * This package provides six powerful utility functions for object manipulation:
- * - pickit: Extract specific keys from objects
- * - modifyit: Modify or erase object properties
- * - mergeit: Merge multiple objects with deep merging support
- * - transformit: Transform object values using mapper functions
- * - omitit: Remove specific keys from objects (inverse of pickit)
- * - flattenit: Flatten nested objects to single-level with dot notation
- *
  * @author Kareem Aboueid
  * @license MIT
  * @homepage https://github.com/kareemaboueid/objeasy
